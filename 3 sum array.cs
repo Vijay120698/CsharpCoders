c98b5456977b4a2eefa7d8c553e63ddf5311d8f9
Vijay120698 <159103685+Vijay120698@users.noreply.github.com>
2024-08-14 11:35:48 +0530

    Add files via upload

---
+++ 3 sum array.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace ThreeIntegerSub
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the size:\t");
+            int Size = int.Parse(Console.ReadLine());
+            int[] nums = new int[Size];
+
+            Console.WriteLine("enter the values:");
+            for (int i = 0; i < nums.Length; i++)
+            {
+                nums[i] = int.Parse(Console.ReadLine());
+            }
+
+            // Sort the array
+            Array.Sort(nums);
+
+            bool found = false;
+
+            for (int i = 0; i < nums.Length - 2; i++)
+            {
+                // Skip duplicates for the first element
+                if (i > 0 && nums[i] == nums[i - 1])
+                {
+                    continue;
+                }
+
+                    int left = i + 1;
+                    int right = nums.Length - 1;
+
+                    while (left < right)
+                    {
+                        int sum = nums[i] + nums[left] + nums[right];
+
+                        if (sum == 0)
+                        {
+                            Console.WriteLine($"[{nums[i]}, {nums[left]}, {nums[right]}]");
+                            found = true;
+
+
+                            // Move the left pointer and skip duplicates
+                            while (left < right && nums[left] == nums[left + 1])
+                                left++;
+                            // Move the right pointer and skip duplicates
+                            while (left < right && nums[right] == nums[right - 1])
+                                right--;
+
+                            // Move the pointers inward
+                            left++;
+                            right--;
+                        }
+                        else if (sum < 0)
+                        {
+                            left++;
+                        }
+                        else
+                        {
+                            right--;
+                        }
+                    }
+                
+            }
+
+            if (!found)
+            {
+                Console.WriteLine("No distinct triplets found.");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
